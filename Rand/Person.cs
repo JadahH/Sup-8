f7f6fbc718886eaea50a83f8ed295380fd070e7f
JadahH <Jhol1918@students.vsu.edu>
2025-02-23 16:35:25 -0500

    implemented person class with ID, name and birthday

---
+++ Rand/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace Rand;
+
+
+/// <summary>
+    /// Represents an individual with an Id, Name, and Birthday.
+    /// </summary>
+
+public class Person
+{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        
+}
+
+
