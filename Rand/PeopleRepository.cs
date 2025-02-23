53181d105befae62126630c9d0e07aa45bf6f55a
JadahH <Jhol1918@students.vsu.edu>
2025-02-23 16:40:09 -0500

    Implemented InitializeData methid using LINQ and Enumerable.Range

---
+++ Rand/PeopleRepository.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rand;
+
+public class PeopleRepository
+    {
+        private Person[] data;
+
+        /// <summary>
+        /// Initializes the 'data' field with 1,000,000 unique Person entries.
+        /// Each Person has a unique Id, a generated Name, and a random Birthday.
+        /// </summary>
+        public void InitializeData()
+        {
+            // Using Enumerable.Range to generate unique Ids from 1 to 1,000,000.
+            data = Enumerable.Range(1, 1000000)
+                .Select(i => new Person
+                {
+                    Id = i,
+                    Name = $"Person{i}",
+                    Birthday = GenerateRandomBirthday()
+                }).ToArray();
+        }
+
+    }
